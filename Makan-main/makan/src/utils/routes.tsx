81a29b5888bbe2820e521a135ea6dbad28baeff3
AsmarJabbarova <145671712+AsmarJabbarova@users.noreply.github.com>
2024-10-22 21:59:39 +0400

    destinationPage/filter
    .

--- Makan-main/makan/src/utils/routes.tsx
+++ Makan-main/makan/src/utils/routes.tsx
@@ -6,15 +6,22 @@ import PlacesPage from "../pages/PlacesPage";
 import ContactPage from "../pages/ContactPage";
 import AboutPage from "../pages/AboutPage";
 import PlaceSinglePage from "../pages/PlaceSinglePage";
+import LoginPage from "../pages/LoginPage";
+import SignUpPage from "../pages/SignUpPage";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App/>,
+        errorElement: <ErrorPage />, 
         children: [
           {
             path: "*",
             element: <ErrorPage />,
+          },
+          {
+            index: true, // This will map the default path `/` to HomePage
+            element: <HomePage />,
           },
           {
             path: '/Home',
@@ -35,6 +42,14 @@ export const router = createBrowserRouter([
           {
             path: '/About',
             element: <AboutPage/>
+          },
+          {
+            path: '/Login',
+            element: <LoginPage/>
+          },
+          {
+            path: '/SignUp',
+            element: <SignUpPage/>
           },
         ]
       },
