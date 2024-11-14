99e788c9996c93f6506d812e40cdb6a1eecc36fd
Raid-dev <a_rauf2004@mail.ru>
2024-11-14 23:37:56 +0400

    Add config folder

---
+++ Backend/config/production.py
@@ -0,0 +1,5 @@
+class Config:
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/mekan'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'e607fabba6613e1a5d00bb5edcc929ac213c494de724ad2d'
